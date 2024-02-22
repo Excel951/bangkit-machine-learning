9090db239b0cb4da8c2a2544f501e16d134927d7
Excel951 <abigailexcelsisdeo@gmail.com>
2024-02-22 23:17:00 +0700

    new changed

--- Google-IT-Automation/Interact-Python-with-OS/Module-2/Reading-writing-csv-file/read-software.py
+++ Google-IT-Automation/Interact-Python-with-OS/Module-2/Reading-writing-csv-file/read-software.py
@@ -1,6 +1,6 @@
 import csv, os
 
 with open("./software.csv", "r") as file:
-    reader = csv.DictReader(file)
+    reader = csv.DictReader(file, strict=True)
     for row in reader:
         print("{} has {} users".format(row["name"], row["users"]))
