0ff669efbeb39a0a4da8918179b6051db765f5ef
Excel951 <abigailexcelsisdeo@gmail.com>
2024-02-24 16:13:50 +0700

    learning regex more deep

--- Google-IT-Automation/Interact-Python-with-OS/Module-3/basic-regex/escaping-character.py
+++ Google-IT-Automation/Interact-Python-with-OS/Module-3/basic-regex/escaping-character.py
@@ -5,3 +5,12 @@ print(re.search(r"\.com", "welcome"))
 print(re.search(r"\.com", "mydomain.com"))
 print(re.search(r"\w*", "This is an example"))
 print(re.search(r"\w*", "And_this_is_another"))
+
+def check_character_groups(text):
+  result = re.search(r"\w\ ", text)
+  return result != None
+
+print(check_character_groups("One")) # False
+print(check_character_groups("123  Ready Set GO")) # True
+print(check_character_groups("username user_01")) # True
+print(check_character_groups("shopping_list: milk, bread, eggs.")) # False
---
+++ Google-IT-Automation/Interact-Python-with-OS/Module-3/basic-regex/regex-action.py
@@ -0,0 +1,24 @@
+import re
+
+print(re.search(r"A.*a", "Argentina"))
+print(re.search(r"A.*a", "Azerbaijan"))
+print(re.search(r"^A.*a$", "Australia"))
+
+# pattern =
+# diawali dengan a-z, A-Z, _
+# diakhiri dengan a-z, A-Z, 0-9, _
+pattern = r"^[a-zA-Z_][a-zA-Z0-9_]*$"
+print(re.search(pattern, "_this_is_a_valid_variable_name"))
+print(re.search(pattern, "this isn't a valid variable"))
+print(re.search(pattern, "my_variable1"))
+print(re.search(pattern, "2my_variable1"))
+
+def check_sentence(text):
+  result = re.search(r"^[A-Z][^.!?]*[.!?]$", text)
+  return result != None
+
+print(check_sentence("Is this is a sentence?")) # True
+print(check_sentence("is this is a sentence?")) # False
+print(check_sentence("Hello")) # False
+print(check_sentence("1-2-3-GO!")) # False
+print(check_sentence("A star is born.")) # True
