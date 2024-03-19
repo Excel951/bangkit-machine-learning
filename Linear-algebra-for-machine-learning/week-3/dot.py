ee234ec7d48046cd0a508d1e11e5502ff0a7d1ea
Excel951 <abigailexcelsisdeo@gmail.com>
2024-03-19 11:34:33 +0700

    learning linear algebra week 3

--- Linear-algebra-for-machine-learning/week-3/dot.py
+++ Linear-algebra-for-machine-learning/week-3/dot.py
@@ -10,3 +10,32 @@ M2 = np.array([
 ])
 
 print(np.dot(M1, M2))
+
+A = np.array([
+    [1,1],
+    [2,2],
+])
+
+print(np.linalg.det(A))
+
+B = np.array([
+    [0.4,-0.2],
+    [-0.2,0.6],
+])
+
+print(np.linalg.det(B))
+
+C = np.array([
+    [0.25,-0.25],
+    [-0.125,0.625],
+])
+
+print(np.linalg.det(C))
+
+D = np.array([
+    [0,0,1],
+    [2,2,1],
+    [1,0,0],
+])
+
+print(np.linalg.det(np.linalg.inv(D)))
